c2aeea597834e8821dfa0c26dafb9c3fe0437760
Mayank Sawhney <mayank.sawhney@gmail.com>
2015-08-01 10:19:31 +0530

    testing visual studio

---
+++ stl/stl/helloSTL.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	std::cout << "Hi STL !" << std::endl;
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+
+	v.clear();
+}
